e9f16774661875c734ca26191d24fdcdaea26927
Samandar2005 <86775489+Samandar2005@users.noreply.github.com>
2023-07-21 17:18:46 +0500

    first commit

---
+++ JS/index.js
@@ -0,0 +1,32 @@
+
+
+
+ let n = prompt("faslni kiriting :")
+
+
+ switch(n){
+    case 12:
+    case 1:
+    case 2:
+     document.write("Winter")
+     break;
+    case 3:
+    case 4:
+    case 5:
+     document.write("Spring")
+     break;
+    case 6:
+    case 7:
+    case 8:
+     document.write("Summer")
+     break;
+    case 9:
+    case 10:
+    case 11:
+     document.write("Autumn")
+     break;
+     
+    default:
+      document.write("ERROR")
+      break;
+   }
